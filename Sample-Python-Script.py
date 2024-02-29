9d4a9a478ac62b73174336f74161f2173ae287d5
Sheldon Dale Cecil <50489459+sheldondc7@users.noreply.github.com>
2024-02-29 13:58:52 +0530

    Create Sample-Python-Script.py

---
+++ Sample-Python-Script.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import json
+
+# These are the credentials passed by the variables of your pipeline to your tasks and in to your env
+CLIENT_ID = os.environ['PORT_CLIENT_ID']
+CLIENT_SECRET = os.environ['PORT_CLIENT_SECRET']
+
+credentials = {
+    'clientId': CLIENT_ID,
+    'clientSecret': CLIENT_SECRET
+}
+token_response = requests.post(f"{API_URL}/auth/access_token", json=credentials)
+access_token = token_response.json()['accessToken']
+
+headers = {
+	'Authorization': f'Bearer {access_token}'
+}
+
+# request url : {API_URL}/blueprints/<blueprint_id>/entities/<entity_id>
+get_response = requests.get(f"{API_URL}/blueprints/test-blueprint/entities/test-entity", headers=headers)
+print(json.dumps(get_response.json(), indent=4))
